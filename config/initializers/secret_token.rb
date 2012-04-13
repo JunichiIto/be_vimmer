f4e2484a6ed71d4ae1d42c0feab477270a9bd8e6
Junichi Ito <june.1st.717@gmail.com>
2012-04-14 04:33:28 +0900

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeVimmer::Application.config.secret_token = '40bc85227276c4fdf9f0674ff22f7cd8cbfc3ebf325bd8bdbb7256212e1cda422faf8e25ef4edc2a70cba4422d4d943f38a9e7631ba3b80a2d605b4f6aa77777'
