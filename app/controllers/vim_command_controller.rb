8ae9af980feb0f13cd2359d999818ce54b3b28f6
Junichi Ito <june.1st.717@gmail.com>
2012-04-15 03:42:14 +0900

    changed to use presence

--- app/controllers/vim_command_controller.rb
+++ app/controllers/vim_command_controller.rb
@@ -1,7 +1,6 @@
 class VimCommandController < ApplicationController
   def index
-    lang = params["lang"]
-    lang ||= "jp"
+    lang = params[:lang].presence || 'jp'
     @vim_commands = VimCommand.find_all_by_language lang
   end
 end
