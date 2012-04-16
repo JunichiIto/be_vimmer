a26ca4d11c2b39c6f5206b0c2d4754841e581bb5
ujihisa <ujihisa@gmail.com>
2012-04-16 15:07:03 -0700

    consistant indentation

--- script/format.rb
+++ script/format.rb
@@ -1,15 +1,15 @@
 # Ad-hoc script to transform Vim documentation
 lines = []
 File.open "en2.txt" do |file|
-    while line = file.gets
-      #if /^ {30,}/ =~ line
-      #  last_line += " " + line.strip
-      #else
-      #  lines << last_line
-      #  last_line = line.chomp
-      #end
-      lines << line.sub(/^ +|\|[^|]+\| +/, "")
-    end
+  while line = file.gets
+    #if /^ {30,}/ =~ line
+    #  last_line += " " + line.strip
+    #else
+    #  lines << last_line
+    #  last_line = line.chomp
+    #end
+    lines << line.sub(/^ +|\|[^|]+\| +/, "")
+  end
 end
 
 lines.each do |line| puts line end
