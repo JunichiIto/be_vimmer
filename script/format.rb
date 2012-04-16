4244c2ab3bf45d49e93ae6e4f1ca40e5503d902c
ujihisa <ujihisa@gmail.com>
2012-04-16 15:11:14 -0700

    do/end to {} because it's not for side-effect

--- script/format.rb
+++ script/format.rb
@@ -1,6 +1,6 @@
 # Ad-hoc script to transform Vim documentation
-lines = File.read("en2.txt").lines.map do |line|
+lines = File.read("en2.txt").lines.map {|line|
   line.sub(/^ +|\|[^|]+\| +/, "")
-end
+}
 
 lines.each do |line| puts line end
