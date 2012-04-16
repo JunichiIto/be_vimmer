cf0f50eef51febb709efbe30e3863281adfc9c47
Junichi Ito <june.1st.717@gmail.com>
2012-04-17 07:27:38 +0900

    fixed migration for Postgres

--- db/migrate/20120416214136_change_mode_id.rb
+++ db/migrate/20120416214136_change_mode_id.rb
@@ -1,14 +1,16 @@
 class ChangeModeId < ActiveRecord::Migration
   def up
+    #http://stackoverflow.com/questions/8503156/altered-my-database-structure-in-development-so-tried-to-reset-heroku-deployed
     remove_index :vim_commands, [:language, :mode_id, :command]
-    change_column :vim_commands, :mode_id, :integer
+    rename_column :vim_commands, :mode_id, :old_mode_id
+    add_column :vim_commands, :mode_id, :integer
+    VimCommand.reset_column_information
+    VimCommand.each {|e| e.update_attribute(:mode_id, e.old_mode_id.to_i) }
+    remove_column :vim_commands, :old_mode_id
     add_index :vim_commands, [:language, :mode_id, :command], unique: true, name: "idx_l_m_c"
   end
 
   def down
-    #However, string type is not valid
-    remove_index :vim_commands, [:language, :mode_id, :command]
-    change_column :vim_commands, :mode_id, :string
-    add_index :vim_commands, [:language, :mode_id, :command], unique: true
+    raise IrreversibleMigration
   end
 end
