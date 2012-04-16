ae1b4d92b6879b7a59e6b15886f9c2e2beeafccd
ujihisa <ujihisa@gmail.com>
2012-04-16 15:10:53 -0700

    each is too verbose

--- script/format.rb
+++ script/format.rb
@@ -1,7 +1,6 @@
 # Ad-hoc script to transform Vim documentation
-lines = []
-File.read("en2.txt").lines.each do |line|
-  lines << line.sub(/^ +|\|[^|]+\| +/, "")
+lines = File.read("en2.txt").lines.map do |line|
+  line.sub(/^ +|\|[^|]+\| +/, "")
 end
 
 lines.each do |line| puts line end
