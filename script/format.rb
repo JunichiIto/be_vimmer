cfb2a58629e09b9c41b460b4a410bb881935c2cf
Junichi Ito <june.1st.717@gmail.com>
2012-04-18 07:16:30 +0900

    reverted manually

--- script/format.rb
+++ script/format.rb
@@ -1,4 +1,12 @@
 # Ad-hoc script to transform Vim documentation
 puts File.readlines("en2.txt").map {|line|
+  #Please keep these lines. --------
+  #if /^ {30,}/ =~ line
+  #  last_line += " " + line.strip
+  #else
+  #  lines << last_line
+  #  last_line = line.chomp
+  #end
+  #---------------------------------
   line.sub(/^ +|\|[^|]+\| +/, "")
 }
