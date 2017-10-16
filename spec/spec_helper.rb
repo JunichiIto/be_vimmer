15fa0bfe0616e861e9c857bb956bd99d4f09b8f0
Junichi Ito <jit@sonicgarden.jp>
2017-10-17 05:54:01 +0900

    Fix spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ require 'rspec/rails'
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
+ActiveRecord::Migration.maintain_test_schema!
+
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
